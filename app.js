1437bb1065e1853e944a1a4f350d30537010a739
Jorge Cossio <jcossio@wfscorp.com>
2019-11-16 10:47:23 -0500

    Add initial graphql support

--- app.js
+++ app.js
@@ -6,10 +6,46 @@ const morgan = require('morgan');
 
 const express = require('express');
 const http = require('http');
+
+const { ApolloServer, gql } = require('apollo-server');
+
 const mongoose = require('mongoose');
 const expressConfig = require('./config/express');
 const routeConfig = require('./routes');
 const config = require('./config/environment');
+
+// gql nos facilita las cosas
+// se le da un nombre ej hello si tiene parametros se le pone parentesis: hello()
+// Luego lo que retorna ejemplo string. Si es obligatorio entonces le ponemos admiracion
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+// Definicion del resolver
+const resolvers = {
+  Query: {
+    // Esto es una funcion
+    // Primer parametro es un root que es el registro actual que estoy trabajando
+    // toda la data del registro que estoy trabajando
+    // Segundo parametro son los parametros que tiene el query si es que tiene.
+    // Tercer parametro es lo que llega desde el servidor para no importar nada
+    // Ejemplo en el contexto pasar si el usuario esta autenticado o no.
+    // Ultimo parametro es la info del query esquema
+    //
+    hello(_, params, context, info) {
+      // Siempre hay que retornar algo segun el esquema
+      return 'Hola MedellinJS'
+    }
+  }
+}
+
+// Graphql necesita un esquema para poder lanzar un servidor. Entonces le pasamos typedefs
+// Y deberia tener un resolver tambien.
+// Aqui tambien se puede especificar un formato de error con:
+// formatError: (err)
+const graphqlServer = new ApolloServer({ typeDefs, resolvers });
 
 // Connect to MongoDB
 mongoose.connect(config.mongo.uri, { useNewUrlParser: true, useFindAndModify: false });
@@ -34,6 +70,21 @@ function startServer() {
   app.shoppingCartBK = server.listen(config.port, config.ip, () => {
     console.log(`Express server listening on ${config.port}, in ${app.get('env')} mode`);
   });
+
+  // Iniciar el servidor de graphql por defecto en el puerto 4000
+  // En esta url graphql nos da un playground.
+  // Aunque postman ya lo soporta
+  graphqlServer.listen().then(( { url }) => console.log(`Server running at ${url}`));
+  // Ya en el playground podemos dar (query o mutacion):
+  // query {
+  //  hello
+  // }
+  // Y responde:
+  // {
+  // "data": {
+  //  "hello": "Hola MedellinJS"
+  //         }
+  // }
 }
 
 setImmediate(startServer);
